5ed6503da3cf558fba16588a2f398089ccf3f24a
Ghidyon <gideonakunana@gmail.com>
2021-09-22 11:07:41 +0300

    seeding database

---
+++ WebApp/Models/SeedData.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApp.Models
+{
+    public static class SeedData
+    {
+        public static void SeedDatabase(DataContext context)
+        {
+            context.Database.Migrate();
+
+            if (context.Products.Count() == 0 && context.Suppliers.Count() == 0 && context.Categories.Count() == 0)
+            {
+                Supplier supplier1 = new Supplier
+                {
+                    Name = "Splash Dudes",
+                    City = "San Jose"
+                };
+
+                Supplier supplier2 = new Supplier
+                {
+                    Name = "Soccer Town",
+                    City = "San Francisco"
+                };
+
+                Supplier supplier3 = new Supplier
+                {
+                    Name = "Chess Co",
+                    City = "New York"
+                };
+
+
+                Category category1 = new Category { Name = "Watersports" };
+
+                Category category2 = new Category { Name = "Soccer" };
+
+                Category category3 = new Category { Name = "Chess" };
+
+                context.Products.AddRange(
+                    new Product {
+                        Name = "Kayak",
+                        Price = 275,
+                        Category = category1,
+                        Supplier = supplier1
+                    },
+
+                    new Product
+                    {
+                        Name = "Lifejacket",
+                        Price = 48.95m,
+                        Category = category1,
+                        Supplier = supplier1
+                    },
+
+                    new Product
+                    {
+                        Name = "Soccer Ball",
+                        Price = 19.50m,
+                        Category = category2,
+                        Supplier = supplier2
+                    },
+                    
+                    new Product
+                    {
+                        Name = "Corner Flags",
+                        Price = 34.95m,
+                        Category = category2,
+                        Supplier = supplier2
+                    },
+                    
+                    new Product
+                    {
+                        Name = "Stadium",
+                        Price = 79500,
+                        Category = category2,
+                        Supplier = supplier2
+                    },
+                    
+                    new Product
+                    {
+                        Name = "Thinking Cap",
+                        Price = 16,
+                        Category = category3,
+                        Supplier = supplier3
+                    },
+                    
+                    new Product
+                    {
+                        Name = "Unsteady Chair",
+                        Price = 29.95m,
+                        Category = category3,
+                        Supplier = supplier3
+                    },
+                    
+                    new Product
+                    {
+                        Name = "Human Chess Board",
+                        Price = 75,
+                        Category = category3,
+                        Supplier = supplier3
+                    },
+                    
+                    new Product
+                    {
+                        Name = "Bling-Bling King",
+                        Price = 1200,
+                        Category = category3,
+                        Supplier = supplier3
+                    });
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -7,19 +7,36 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using WebApp.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebApp
 {
     public class Startup
     {
+        private IConfiguration Configuration { get; set; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // Add DbContext Services
+            services.AddDbContext<DataContext>(options => 
+            {
+                options.UseSqlServer(Configuration["ConnectionStrings:ProductConnection"]);
+                options.EnableSensitiveDataLogging(true);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, DataContext context)
         {
             if (env.IsDevelopment())
             {
@@ -35,6 +52,8 @@ namespace WebApp
                     await context.Response.WriteAsync("Hello World!");
                 });
             });
+
+            SeedData.SeedDatabase(context);
         }
     }
 }
